22782c3e3fdd8a60b185a9ac7872c3dca9df65a7
Mohamed Lamara <m.l.ouzrourou@gmail.com>
2026-08-10 12:03:09 +0100

    Implement "User Input" application layer module

--- firmware/include/app/user_input.h
+++ firmware/include/app/user_input.h
@@ -1,6 +1,18 @@
 #ifndef USER_INPUT_H
 #define USER_INPUT_H
 
+#include <stdbool.h>
 
+typedef enum
+{
+    USER_INPUT_INCREMENT,
+    USER_INPUT_DECREMENT,
+    USER_INPUT_SELECT,
+    USER_INPUT_START_FINISH,
+    USER_INPUT_RESET,
+    USER_INPUT_MODE
+} UserInput_t;
+
+bool UserInput_IsPressed(UserInput_t input);
 
 #endif
